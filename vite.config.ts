37712c44af6a441003c14bdb0bfd8b0404a5bebd
AerfaKk <1025783794@qq.com>
2026-03-01 20:30:49 +0800

    fix: 用相对路径解决404

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Vue3-demo/',
+  base: './',
   plugins: [
     vue(),
     vueDevTools(),
